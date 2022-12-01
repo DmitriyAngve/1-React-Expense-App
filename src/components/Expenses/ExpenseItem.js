beb3b21380957aad5b03bde092306f5f6dbfe5e4
Dmitriy Angve <kirirch129@gmail.com>
2022-12-01 15:06:19 +0300

    #

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -10,7 +10,7 @@ const ExpenseItem = (props) => {
 
   //useState rigisters sime State, same value as a State for the component in which it is being called
   const [title, setTitle] = useState(props.title);
-  console.log("ExpenseItem evaluated by React"); // we will see it 4 times by the number of components
+  console.log("ExpenseItem evaluated by React"); // we see that 4 times
 
   const clickHandler = () => {
     // console.log("CLICKED!!!");
@@ -22,7 +22,7 @@ const ExpenseItem = (props) => {
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,11 +5,15 @@ import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState("2022");
+  const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
   return (
     <div>
@@ -18,7 +22,16 @@ const Expenses = (props) => {
           selected={filteredYear}
           onfilterChangeHandler={filterChangeHandler}
         />
-        <ExpenseItem
+        {filteredExpenses.map((expense) => (
+          <ExpenseItem
+            // helps React identify the individual items
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+        {/* <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
           date={props.items[0].date}
@@ -37,7 +50,7 @@ const Expenses = (props) => {
           title={props.items[3].title}
           amount={props.items[3].amount}
           date={props.items[3].date}
-        />
+        /> */}
       </Card>
     </div>
   );
